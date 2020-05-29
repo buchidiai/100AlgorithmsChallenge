2c0e07a03887c31098c4b38d6f01db78e2bf60f7
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-29 16:25:47 -0700

    added border excercise

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,34 @@
 function addBorder(picture: string[]): string[] {
+  const border: number = picture[0].length + 3;
+  let wall: string = "";
 
+  for (let i = 0; i < border; i++) {
+    wall += "*";
+  }
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = picture[i] + "*";
+    picture[i] = "*" + picture[i];
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+function addBorder2(picture: string[]): string[] {
+  let wall: string = "*".repeat(picture[0].length + 3);
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+  }
+
+  return picture;
+}
+
+console.log(addBorder(["i am the best", "You are not"]));
+console.log(addBorder2(["i am the best", "You are not"]));

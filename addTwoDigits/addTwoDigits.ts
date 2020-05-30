10f3eff0dcc2140c516407144935803469e83c2c
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-29 17:11:41 -0700

    added twodigits  excercise

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
-function addTwoDigits(n: any): number {
+function addTwoDigits(n: number): number {
+  let result: number = 0;
+  if (n < 10) {
+    return n;
+  }
 
+  for (const i of n.toString().split("")) {
+    result += parseInt(i);
+  }
+
+  return result;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(98765));

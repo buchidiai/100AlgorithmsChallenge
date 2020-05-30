cccfc1caaa2378614d630f0bb8435ec835a0eb3b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-30 13:32:53 -0700

    added all longeststrings excercise

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -5,10 +5,8 @@ function adjacentElementsProduct(inputArray: number[]): any {
   let product = [];
   let largest = 0;
 
-  for (let i = 0; i < inputArray.length; i += 2) {
-    if (inputArray[i + 1] !== undefined) {
-      product.push(inputArray[i] * inputArray[i + 1]);
-    }
+  for (let i = 0; i < inputArray.length - 1; i += 2) {
+    product.push(inputArray[i] * inputArray[i + 1]);
   }
 
   for (const prod of product) {
@@ -19,4 +17,24 @@ function adjacentElementsProduct(inputArray: number[]): any {
   return largest;
 }
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+//more concise
+function adjacentElementsProduct1(inputArray: number[]): any {
+  if (inputArray.length % 2 !== 0) {
+    return "must be even array";
+  }
+
+  //initialize largest product
+  let largestProduct = inputArray[0] * inputArray[1];
+
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    //product of two numbers
+    const product = inputArray[i] * inputArray[i + 1];
+
+    //if largestproduct is less than product  --> return value
+    largestProduct = largestProduct < product ? product : largestProduct;
+  }
+
+  return largestProduct;
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -59, 7, 3]));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,14 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let max = 0;
+  let array = [];
 
+  for (const i of inputArray) {
+    if (i.length >= max) {
+      max = i.length;
+      array.push(i);
+    }
+  }
+  return array;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));

6f729e18645aad817cd9d8ef36965f7d3f519fde
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-30 16:16:04 -0700

    added alphabeticShift excercise

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,45 @@
 function alphabeticShift(inputString: string): string {
+  const alphabet: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
+  let index = 0;
+  let newString = "";
+
+  for (let i of inputString.split("")) {
+    if (i !== "z") {
+      index = alphabet.indexOf(i) + 1;
+    }
+
+    newString += alphabet[index];
+  }
+
+  return newString;
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));

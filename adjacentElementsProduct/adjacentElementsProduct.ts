09cbc47f36218e068f840f395254e31c98cd3763
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-29 19:47:09 -0700

    added adjacentElementsProduct excercise

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,22 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray: number[]): any {
+  if (inputArray.length % 2 !== 0) {
+    return "must be even array";
+  }
+  let product = [];
+  let largest = 0;
 
+  for (let i = 0; i < inputArray.length; i += 2) {
+    if (inputArray[i + 1] !== undefined) {
+      product.push(inputArray[i] * inputArray[i + 1]);
+    }
+  }
+
+  for (const prod of product) {
+    if (prod > largest) {
+      largest = prod;
+    }
+  }
+  return largest;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));

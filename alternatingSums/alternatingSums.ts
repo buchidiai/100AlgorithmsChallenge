baaa0abf183ef4f2300922f8c0e5ac2f16440bd1
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-31 17:03:24 -0700

    added alternatingSums excercise

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,17 @@
 function alternatingSums(a: number[]): number[] {
+  let result: number[] = [];
+  let team1 = 0;
+  let team2 = 0;
 
+  for (let i = 0; i < a.length; i += 2) {
+    team1 += a[i];
+    if (a[i - 1] !== undefined) {
+      team2 += a[i - 1];
+    }
+  }
+  result.push(team1, team2);
+
+  return result;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));

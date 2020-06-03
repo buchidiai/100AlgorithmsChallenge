992adca433e92254fac92793eef3f685f025726b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-03 15:45:57 -0700

    added centuryFromYear excercise

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,15 @@
 function centuryFromYear(year: number): number {
- 
+  const theYear = year.toString();
+
+  if (theYear.split("").length <= 3) {
+    return parseInt(theYear[0]);
+  }
+
+  let start = parseInt(theYear.slice(0, 2));
+  let end = parseInt(theYear.slice(2, 4));
+
+  return end >= 1 ? start + 1 : start;
 }
 
-console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1500));
 console.log(centuryFromYear(1700));

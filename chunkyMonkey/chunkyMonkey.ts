18a9d35ac4e2e16cadf8d21fd6acd7bf93b9fa6d
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-04 10:09:29 -0700

    fix chunkyMonkey excercise

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,16 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
+  let newArray = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let lastItemInArray = newArray[newArray.length - 1];
+
+    !lastItemInArray || lastItemInArray.length === size
+      ? newArray.push([arr[i]])
+      : lastItemInArray.push(arr[i]);
+  }
+
+  return newArray;
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));

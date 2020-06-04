16a766dd26c3351c1423cd821260324fd08a82fc
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-04 15:28:01 -0700

    added compareIntegers excercise

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,10 @@
 function compareIntegers(a: string, b: string): string {
+  const intA = parseInt(a);
+  const intB = parseInt(b);
 
+  return intA > intB ? "greater" : intA >= intB ? "equal" : "less";
 }
 
-console.log(compareIntegers('12', '13'));
-console.log(compareIntegers('875', '799'));
-console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers("12", "13"));
+console.log(compareIntegers("875", "799"));
+console.log(compareIntegers("1000", "1000"));

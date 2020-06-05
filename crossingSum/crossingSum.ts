1d8b16867e16a1681fdaacdc5c26954fffc137d2
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-05 16:26:05 -0700

    added crossingSum excercise

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,27 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+  const columnTotal = matrix[a].reduce((a, b) => a + b);
+  console.log(columnTotal);
+
+  let rowTotal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i][b]);
+
+    rowTotal += i !== a ? matrix[i][b] : 0;
+  }
+  console.log(rowTotal);
+
+  return columnTotal + rowTotal;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+    ],
+    1,
+    3
+  )
+);

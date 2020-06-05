4bb148417ca537c6c1909fffb49e7ac48480e609
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-05 12:55:52 -0700

    added confirmEnding excercise

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,20 @@
 function confirmEnding(str: string, target: string) {
-   
+  const string = str.split("");
+  const targetString = target.split("");
+  let result = "";
+
+  for (let i = string.length - 1; i >= targetString.length - 1; i--) {
+    result += string[i];
+  }
+
+  return result.split("").reverse().join("") === target;
 }
 
-console.log(confirmEnding("Abstraction", "action"));
+function confirmEnding1(str: string, target: string) {
+  const start = str.length - target.length;
+
+  return str.substr(start) === target;
+}
+
+console.log(confirmEnding1("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));

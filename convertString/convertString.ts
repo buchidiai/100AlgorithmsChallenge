e94e3765b35ba04151574f9103c3dc69a2fb1dfb
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-05 14:38:59 -0700

    added convertString excercise

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,25 @@
 function convertString(s: string, t: string): boolean {
+  let origin = buildMap(s);
+  let other = buildMap(t);
+  let count = 0;
 
+  for (const [k, v] of Object.entries(origin)) {
+    if (other[k]) {
+      count++;
+    }
+  }
+  return count === t.split("").length;
 }
 
-console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-console.log(convertString('addbyca', 'abcd'));
+const buildMap = (str) => {
+  let map = {};
+  for (const i of str.split("")) {
+    map[i] = map[i] + 1 || 1;
+  }
+
+  return map;
+};
+
+console.log(convertString("ceoydefthf5iyg5h5yts", "codefights"));
+console.log(convertString("addbyca", "abcd"));
+console.log(convertString("abcdgfytfffghjjjk", "jftz"));

be6d1a2d5c85f49955d20ad78ec58379030e4b81
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-05 18:20:30 -0700

    added depositProfit excercise

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,18 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let increasedAmount = deposit;
+  let year = 0;
+
+  while (threshold > increasedAmount) {
+    increasedAmount += increasedAmount * (rate / 100);
+
+    year++;
+  }
+
+  return year;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));

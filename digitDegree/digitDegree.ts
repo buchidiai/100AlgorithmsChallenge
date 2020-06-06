dd4ed84a50b0a44c9aa0fa518f34957d4425b486
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-05 19:33:46 -0700

    unfinished digitDegree excercise

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,26 @@
 function digitDegree(n: number): number {
-  
+  if (n < 10) {
+    return n;
+  }
+
+  let answer = 0;
+  let next = 0;
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  while (answer !== 1) {
+    let arrayNumbers = numberToArray(n);
+    console.log(arrayNumbers);
+    let result = arrayNumbers.reduce(reducer);
+
+    return result === 1 ? (answer = result) : result;
+  }
 }
 
-console.log(digitDegree(5));
-console.log(digitDegree(10));
+function numberToArray(n) {
+  return Array.from(String(n), Number);
+}
+
+// console.log(digitDegree(5));
+//console.log(digitDegree(10));
 console.log(digitDegree(91));

ae090948106b99d7ea3507dce1c02a251b1b3d27
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 12:32:33 -0700

    added evenDigitsOnly  excercise

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,15 @@
 function evenDigitsOnly(n: number): boolean {
+  const arrayNumbers = Array.from(String(n), Number);
 
+  console.log(arrayNumbers);
+
+  for (const i of arrayNumbers) {
+    if (i % 2 !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 console.log(evenDigitsOnly(248622));

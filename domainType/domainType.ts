7f0ce49d89e3c1b7150dcbf05680e95e34a41f4b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 11:13:46 -0700

    added domainType excercise

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,27 @@
 function domainType(domains: string[]): string[] {
+  let result = [],
+    items,
+    type;
 
+  for (const i of domains) {
+    items = i.split(".");
+    type = items[items.length - 1];
+    console.log(type);
+
+    if (type === "org") {
+      result.push("organization");
+    } else if (type === "com") {
+      result.push("commercial");
+    } else if (type === "net") {
+      result.push("network");
+    } else if (type === "info") {
+      result.push("information");
+    }
+  }
+
+  return result;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"])
+);

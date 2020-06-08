6e4311adb048c1617620c887a07e814bc01af4c7
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 14:09:31 -0700

    added factorializeANumber excercise

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,18 @@
 function factorializeANumber(num: number): number {
+  if (num <= 2) return num;
 
+  return factorializeANumber(num - 1) * num;
+}
+
+function factorializeANumber1(num: number): number {
+  let answer = 1;
+
+  for (let i = 1; i <= num; i++) {
+    answer *= i;
+  }
+
+  return answer;
 }
 
 console.log(factorializeANumber(5));
-console.log(factorializeANumber(10));
+console.log(factorializeANumber1(10));

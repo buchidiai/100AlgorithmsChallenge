0327cf188a573521f86c0ef8db76cc3a83b05ea2
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 13:12:33 -0700

    added extractMatrixColumn excercise

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,20 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+  let result = [];
 
+  for (let i = 0; i < matrix.length; i++) {
+    result.push(matrix[i][column]);
+  }
+
+  return result;
 }
 
-console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+  extractMatrixColumn(
+    [
+      [1, 1, 1, 2],
+      [0, 5, 0, 4],
+      [2, 1, 3, 6],
+    ],
+    2
+  )
+);

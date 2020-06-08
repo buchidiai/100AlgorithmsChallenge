8dffe936d53370c7d2bb5832ffbd97731e72f57e
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 13:06:40 -0700

    added extractEachKth excercise

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,18 @@
-function extractEachKth(inputArray:number[], k: number): number[] {
+function extractEachKth(inputArray: number[], k: number): number[] {
+  return inputArray.filter((nums, index) => (index + 1) % k !== 0);
+}
+
+function extractEachKth1(inputArray: number[], k: number): number[] {
+  let result = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if ((i + 1) % 3 !== 0) {
+      result.push(inputArray[i]);
+    }
+  }
+
+  return result;
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+//

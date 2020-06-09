7f48a4239ce610c7699bd62782cbd13ec23c6143
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-09 11:30:46 -0700

    added firstDuplicate excercise

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,15 @@
 function firstDuplicate(a: number[]): number {
-    
+  let map = {};
+
+  for (let i = 0; i < a.length; i++) {
+    if (map.hasOwnProperty(a[i])) {
+      return a[i];
+    }
+
+    map[a[i]] = map[a[i]] + 1 || 1;
+  }
+
+  return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));

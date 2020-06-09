b61bed7cd03ddd9bf8438393b98b844b190e0bfd
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 18:48:17 -0700

    unfinished findClosestPair excercise

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,12 +1,18 @@
-function findClosestPair(numbers: number[], sum: number): number[] {
+function findClosestPair(numbers: number[], sum: number): number {
+  let distance = -1;
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1; j < numbers.length - 1; j++) {
+      const absDistance = Math.abs(i - j);
+
       if (numbers[i] + numbers[j] === sum) {
-        return [numbers[i], numbers[j]];
+        if (distance === -1 || absDistance < distance) {
+          distance = absDistance;
+        }
       }
     }
   }
+  return distance;
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-// console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([2, 3, 7], 8));

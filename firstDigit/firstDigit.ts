4af8f43ee0a60af6e67113eebe46dfa710dea4a6
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-08 19:02:30 -0700

    unfinished firstDigit excercise

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,24 @@
 function firstDigit(inputString: string): string {
+  let string = inputString.split("");
 
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(parseInt(string[i]))) {
+      return string[i];
+    }
+  }
 }
 
-console.log(firstDigit('var_1__Int'));
-console.log(firstDigit('q2q-q'));
-console.log(firstDigit('0ss'));
+function firstDigit1(inputString: string): string {
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const chars = inputString.split("");
+
+  for (let i = 0; i < chars.length; i++) {
+    if (digits.includes(chars[i])) {
+      return chars[i];
+    }
+  }
+}
+
+console.log(firstDigit("var_1__Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit1("0ss"));

a9458d21a3579e4b55fd732205d15f5dd790243b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 16:09:28 -0700

    added htmlEndTagByStartTag excercise

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,19 @@
 function htmlEndTagByStartTag(startTag: string): string {
+  let tagArray = startTag.split(" ");
+  let tagArray2 = tagArray[0].toString().split("");
 
+  let endTag = "</";
+
+  console.log(tagArray2);
+
+  for (let i = 1; i < tagArray2.length; i++) {
+    console.log(endTag);
+
+    endTag += tagArray2[i];
+  }
+
+  return endTag[endTag.length - 1] === ">" ? endTag : (endTag += ">");
 }
-
+//console.log(htmlEndTagByStartTag("<img src>"));
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+//console.log(htmlEndTagByStartTag("<i>"));

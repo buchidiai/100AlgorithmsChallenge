5f9a4020f19fdcb918d63d95f9c1b98e789cdc05
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 13:24:24 -0700

    added houseNumbersSum excercise

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,11 @@
 function houseNumbersSum(inputArray: number[]): number {
+  let result = 0;
 
+  for (const i of inputArray) {
+    if (i === 0) return result;
+
+    result += i;
+  }
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));

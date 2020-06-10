d995477551379609697e25449fd81f9b728aacd3
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 13:18:32 -0700

    added growingPlant excercise

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,21 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  let speed = 0;
+  let days = 0;
 
+  while (speed < desiredHeight) {
+    speed += upSpeed;
+    days++;
+
+    if (speed < desiredHeight) {
+      speed -= downSpeed;
+    }
+  }
+
+  return days;
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));

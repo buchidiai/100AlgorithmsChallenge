4fb017049ccbc8599c167403abbfe93b3069cc60
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 14:27:55 -0700

    added houseOfCats excercise

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,5 +1,13 @@
 function houseOfCats(legs: number): number[] {
+  const cat = 4,
+    person = 2,
+    result = [];
 
+  Math.floor(legs / cat) <= 0 ? null : result.push(Math.floor(legs / cat));
+
+  result.push(Math.floor(legs / person));
+
+  return result;
 }
 
 console.log(houseOfCats(6));

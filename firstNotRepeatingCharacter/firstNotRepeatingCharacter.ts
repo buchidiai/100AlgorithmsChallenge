a400c8ac2d637f3b9d15e8df70d57bf14963510e
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-09 17:55:12 -0700

    added firstNotRepeatingCharacter excercise

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,13 @@
 function firstNotRepeatingCharacter(s: string): string {
-
+  for (var i = 0; i < s.length; i++) {
+    var c = s.charAt(i);
+    if (s.indexOf(c) === i && s.indexOf(c, i + 1) === -1) {
+      return c;
+    }
+  }
+  return "_";
 }
 
-console.log(firstNotRepeatingCharacter('abacabad'));
-console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacabaag"));
+console.log(firstNotRepeatingCharacter("abacabaabacabaa"));

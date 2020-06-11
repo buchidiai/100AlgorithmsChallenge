9874dffcee8e3ed78e9ee285405057eaa8545f86
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 11:37:40 -0700

    added lateRide excercise

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -5,7 +5,6 @@ function largestOfFour(nums: number[][]): number[] {
   for (const level0 of nums) {
     for (const level1 of level0) {
       largest = largest < level1 ? level1 : largest;
-      console.log(level1);
     }
     result.push(largest);
   }
@@ -21,11 +20,11 @@ console.log(
     [1000, 1001, 857, 1],
   ])
 );
-// console.log(
-//   largestOfFour([
-//     [4, 9, 1, 3],
-//     [13, 35, 18, 26],
-//     [32, 35, 97, 39],
-//     [1000000, 1001, 857, 1],
-//   ])
-// );
+console.log(
+  largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1],
+  ])
+);
--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,28 @@
 function lateRide(n: number): number {
+  let minInHour = 60;
+  let result = [];
 
+  if (n % minInHour === 0) {
+    result.push(n / minInHour);
+  } else {
+    let hour = stringArrToNum(
+      Math.floor(n / minInHour)
+        .toString()
+        .split("")
+    );
+
+    let mins = stringArrToNum((n % minInHour).toString().split(""));
+
+    result.push(...hour, ...mins);
+  }
+
+  return result.reduce((a, b) => a + b);
+}
+
+function stringArrToNum(arr) {
+  return Array.from(arr, Number);
 }
 
 console.log(lateRide(240));
 console.log(lateRide(808));
+console.log(lateRide(350));

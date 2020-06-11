6a78f670e458d4f57cb5dec9916e078df81ade91
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 13:08:03 -0700

    added longestDigitsPrefix excercise

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,16 @@
 function longestDigitsPrefix(inputString: string): string {
+  let string = inputString.split("");
+  let result = "";
 
+  for (let i = 0; i < string.length; i++) {
+    if (parseInt(string[i]) && !isNaN(parseInt(string[i]))) {
+      result += string[i];
+    } else {
+      return result;
+    }
+  }
+  return result;
 }
 
-console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa1"));
+console.log(longestDigitsPrefix("9345345admsd"));

e79c8c095c6bf743824e8500a0dd16462f89324d
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 14:28:14 -0700

    added makeArrayConsecutive2 excercise

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,18 @@
 function makeArrayConsecutive2(statues: number[]): number {
+  const max = Math.max(...statues);
+  console.log(max);
 
+  const min = Math.min(...statues);
+  console.log(min);
+  const completeArray = [];
+
+  for (let i = min; i <= max; i++) {
+    console.log(i);
+
+    completeArray.push(i);
+  }
+
+  return completeArray.length - statues.length;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));

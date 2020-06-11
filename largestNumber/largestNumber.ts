cbb96a46e23258afa71d6c978089e86fe4fdb861
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 18:10:45 -0700

    added largestOfFour excercise

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,10 @@
 function largestNumber(n: number): number {
- 
+  let word = "";
+  for (let i = 0; i < n; i++) {
+    word += "9";
+  }
+
+  return parseInt(word);
 }
 
-console.log(largestNumber(2));
+console.log(largestNumber(5));
--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,6 +1,31 @@
 function largestOfFour(nums: number[][]): number[] {
+  const result = [];
+  let largest = 0;
 
+  for (const level0 of nums) {
+    for (const level1 of level0) {
+      largest = largest < level1 ? level1 : largest;
+      console.log(level1);
+    }
+    result.push(largest);
+  }
+
+  return result;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
-console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
+// console.log(
+//   largestOfFour([
+//     [4, 9, 1, 3],
+//     [13, 35, 18, 26],
+//     [32, 35, 97, 39],
+//     [1000000, 1001, 857, 1],
+//   ])
+// );

7095b6ab2ee13458ccf7f4616aae322fcf3d8f50
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 17:34:39 -0700

    added isLucky excercise

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,12 @@
 function isLucky(n: number): boolean {
-    
+  const numArray = Array.from(String(n), Number);
+  const len = numArray.length;
+  const firstHalf = numArray.slice(0, len / 2);
+  const rest = numArray.slice(len / 2, len);
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  return firstHalf.reduce(reducer) === rest.reduce(reducer);
 }
 
 console.log(isLucky(1230));

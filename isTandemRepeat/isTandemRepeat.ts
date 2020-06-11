84c11625c1d3644471e74a0472e966572c13005a
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-10 17:53:40 -0700

    added isTandemRepeat excercise

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,13 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+  const strArr = inputString.split("");
+  const len = strArr.length;
+
+  const firstHalf = strArr.slice(0, len / 2).join("");
+  const rest = strArr.slice(len / 2, len).join("");
+
+  return firstHalf === rest;
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat("tandemtandem"));
+console.log(isTandemRepeat("qqq"));
+console.log(isTandemRepeat("2w2ww"));

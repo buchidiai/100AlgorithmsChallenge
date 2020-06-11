c57ac16b4ce7cf96e774b09cb1e8f9afa331f2e9
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 15:24:08 -0700

    added missingLetters excercise

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,6 +1,18 @@
 function missingLetters(str: string): string {
-   
+  const alphabet: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+  const inputString = str.split("");
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] !== inputString[i]) {
+      return alphabet[i];
+    }
+  }
+
+  return undefined;
 }
+
+const toArray = (str) => str.split("");
+
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
 console.log(missingLetters("abcdefghjklmno"));

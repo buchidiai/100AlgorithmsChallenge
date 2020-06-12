a9f7e2b078fe5571af8a5e500ae6ba321223f243
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 17:53:50 -0700

    added properNounCorrection excercise

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,12 @@
 function properNounCorrection(noun: string): string {
-   
+  let inputAray = noun.split("");
+  let result = inputAray[0].toUpperCase();
+
+  for (let i = 1; i < noun.length; i++) {
+    result += noun[i].toLowerCase();
+  }
+  return result;
 }
 
-console.log(properNounCorrection('pARiS'));
-console.log(properNounCorrection('John'));
+console.log(properNounCorrection("pARiS"));
+console.log(properNounCorrection("John"));

bf3671713f036358a2aaea987db6977626d0f880
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 17:02:33 -0700

    added pagesNumberingWithInk excercise

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,3 +1,21 @@
-function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+function pagesNumberingWithInk(
+  current: number,
+  numberOfDigits: number
+): number {
+  let limit = current + numberOfDigits;
 
+  let result = 0;
+
+  for (let i = current; i < limit; i++) {
+    let itemLength = i.toString().length;
+    result += itemLength;
+
+    if (result >= numberOfDigits) {
+      return result > numberOfDigits ? i - 1 : i;
+    }
+  }
 }
+
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));

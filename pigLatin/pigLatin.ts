4b06879010bc17cdf809ddeb321a685bb4d864d8
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 17:25:17 -0700

    added pigLatin excercise

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,25 @@
 function pigLatin(str: string): string {
+  const chars = str.split("");
+  console.log(chars);
 
+  const vowelRegex = /[aeiou]/;
+
+  if (vowelRegex.test(str[0])) {
+    return `${str}way`;
+  }
+
+  while (true) {
+    if (!vowelRegex.test(chars[0])) {
+      chars.push(chars.splice(0, 1)[0]);
+    } else {
+      break;
+    }
+  }
+
+  str = chars.join("") + "ay";
+
+  return str;
 }
 
-console.log(pigLatin("glove"));
+//console.log(pigLatin("glove"));
 console.log(pigLatin("eight"));

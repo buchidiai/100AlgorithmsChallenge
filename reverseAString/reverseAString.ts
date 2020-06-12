a9d8811e27a8722f33243e4104fc141e9a8bf93d
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-11 18:31:14 -0700

    added reverseAString excercise

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,22 @@
 function reverseAString(str: string): string {
-
+  return str.split("").reverse().join("");
 }
 
-console.log(reverseAString('hello'));
-console.log(reverseAString('Howdy'));
+function reverseAString2(str: string): string {
+  let result = "";
+  let inputString = str.split("");
+
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    console.log(inputString[i]);
+
+    result += inputString[i];
+  }
+
+  return result;
+}
+
+console.log(reverseAString("hello"));
+console.log(reverseAString("Howdy"));
+
+console.log(reverseAString2("hello"));
+console.log(reverseAString2("Howdy"));

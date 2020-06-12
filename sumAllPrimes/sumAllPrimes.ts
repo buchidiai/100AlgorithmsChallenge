a41abc3e12747386fa9fe509c08d1509abf86971
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-06-12 12:50:17 -0700

    added sumAllPrimes excercise

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,22 @@
 function sumAllPrimes(num: number): number {
+  var answer = 0;
 
+  for (var i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+function isPrime(val) {
+  //test if number is prime
+  for (var i = 2; i <= val / 2; i++) {
+    if (val % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(sumAllPrimes(10));
